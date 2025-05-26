3713c85fc1df3ad93163bc1e20d473b8eef32eab
Joshua Snyder <josh@cheetahfox.com>
2025-05-26 04:07:17 +0000

    Just some initial work

---
+++ main.go
@@ -0,0 +1,31 @@
+/*
+Simple Go Program to locacte which server ceph is running it's managed Prometheus.
+
+This might only be useful for me; since I want to pull metrics from a ceph managed
+Prometheus server and I don't want to make any modifications to the ceph cluster. I
+just want to use what they are already running. This issue is that ceph can run this
+on any of the nodes in the cluster. If something changes in the cluster it can move
+to a new node without warning and it will break my monitoring setup.
+
+Could I solve this differently? Yeah, I do different things like scraping the Prometheus
+node exporters directly, but it's nice that ceph manges everything so I just need to
+pull the metrics from one place.
+*/
+package main
+
+import (
+	"fmt"
+
+	"github.com/ansrivas/fiberprometheus/v2"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Println("Starting ceph prometheus locator...")
+	locator := fiber.New()
+
+	prometheus := fiberprometheus.New("ceph_prometheus_locator")
+	prometheus.RegisterAt(locator, "/metrics")
+	locator.Use(prometheus.Middleware)
+
+}
